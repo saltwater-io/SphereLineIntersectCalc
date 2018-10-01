a44f3cbb153e411dce72deab6390bacec4a5d0a1
Dakota McGuire <30912971+saltwater-io@users.noreply.github.com>
2018-10-01 12:46:57 -0500

    Add files via upload

--- SphereIntersect.py
+++ SphereIntersect.py
@@ -12,14 +12,14 @@ def getSphereIntersects(v1, v2, v3):
     radius = 3
 
     # a = (x2 - x1)^2 + (y2 - y1)^2 + (z2 - z1)^2
-    a = squareNum(v2[0] - v1[0]) + squareNum(v2[1] - v1[1]) + squareNum(v2[2] - v1[2])
+    a = squareNum(v2[0]-v1[0])+squareNum(v2[1]-v1[1])+squareNum(v2[2]-v1[2])
 
     # b = 2( (x2 - x1) (x1 - x3) + (y2 - y1) (y1 - y3) + (z2 - z1) (z1 - z3) )
-    b = 2 * ((v2[0] - v1[0]) * (v1[0] - v3[0]) + (v2[1] - v1[1]) * (v1[1] - v3[1]) + (v2[2] - v1[2]) * (v1[2] - v3[2]))
+    b = 2 * ((v2[0]-v1[0]) * (v1[0]-v3[0])+(v2[1]-v1[1]) * (v1[1]-v3[1])+(v2[2]-v1[2])*(v1[2]-v3[2]))
 
     #  c = (x3)^2 + (y3)^2 + (z3)^2 + (x1)^2 + (y1)^2 + (z1)^2 - 2[x3*x1 + y3*y1 + z3*z1] - r2
-    c = (squareNum(v3[0]) + squareNum(v1[1]) + squareNum(v3[2]) + squareNum(v1[0]) +
-         squareNum(v1[1]) + squareNum(v1[2]) - 2 * (v3[0] * v1[0] + v3[1] * v1[1] + v3[2] * v1[2]) - squareNum(radius))
+    c = (squareNum(v3[0])+squareNum(v1[1]) + squareNum(v3[2])+squareNum(v1[0]) +
+         squareNum(v1[1]) + squareNum(v1[2]) - 2*(v3[0]*v1[0]+v3[1] * v1[1]+v3[2]*v1[2])-squareNum(radius))
 
     checkMe = (b*b-4*a*c)  # Inside quadratic formula, this determines the number of intersects
 
@@ -35,19 +35,19 @@ def getSphereIntersects(v1, v2, v3):
                             #               |    divided by 2a     |
             # first intersection
             mod = (-b + sqrt(checkMe)) / (2 * a)
-            Intersect1 = [v1[0] + mod * (v2[0] - v1[0]), v1[1] + mod * (v2[1] - v1[1]), v1[2] + mod * (v2[2] - v1[2])]
+            Intersect1 = [v1[0]+mod*(v2[0]-v1[0]), v1[1]+mod*(v2[1]-v1[1]), v1[2]+mod*(v2[2]-v1[2])]
 
             # second intersection
             mod = (-b - sqrt(checkMe)) / (2 * a)
-            Intersect2 = [v1[0] + mod * (v2[0] - v2[0]), v1[1] + mod * (v2[1] - v1[1]), v1[2] + mod * (v2[2] - v1[2])]
+            Intersect2 = [v1[0]+mod*(v2[0]- v2[0]), v1[1]+mod*(v2[1]-v1[1]), v1[2]+mod*(v2[2]-v1[2])]
 
             print("Intersect #1: " + getString(Intersect1))
             print("Intersect #2: " + getString(Intersect2))
 
-        elif checkMe == 0:  # one intersection
+        elif checkMe == 0:
+            # one intersection
             mod = (-b / (2 * a))
-
-            Intersect1 = [v1[0] + mod * (v2[0] - v1[0]), v1[1] + mod * (v2[1] - v1[1]), v1[2] + mod * (v2[2] - v1[2])]
+            Intersect1 = [v1[0] + mod * (v2[0]-v1[0]), v1[1]+mod*(v2[1]-v1[1]), v1[2]+mod *(v2[2]-v1[2])]
 
             print("One Intersect: " + getString(Intersect1))
 
